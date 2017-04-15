7fb63540bcdcb39549119e9cb9ff5fd587ff4e1d
Jully Nobre <jullynobre1@gmail.com>
2017-04-15 16:08:05 -0300

    Revert "ashahdkj"
    This reverts commit 44fd0a61982f6110760e0378d2b4bcf8050de77e.

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import turtle
 import helper
+import draw
 
+
+def x_generator():
+    num_x=turtle.window_width()/2
+    a=(num_x/2)*(-1)
+    x=[]
+    for i in range(int(num_x)):
+        x.append(a)
+        a+=1
+    return x
+
+
+def y_generator(x,func):
+    y=[]
+    for i in range(len(x)):
+        y.append(get_y(func, x[i]))
+    return y
 
 def main():
     scn = turtle.Screen()
     text = scn.textinput("Teste", "Teste Teste")
-    text = helper.clear_function(text)
-    x = x_generator()
-    y = y_generator(text, x)
-    print(y)
-
-
-def x_generator():
-    num_x = turtle.window_width()/2
-    a = (num_x/2)*(-1)
-    x = []
-    for i in range(int(num_x)):
-        x.append(a)
-        a += 1
-    return x
-
-
-def y_generator(func, x):
-    y = []
-    for i in range(len(x)):
-        y.append(helper.get_y(func, x[i]))
-    return y
+    print(helper.get_y(text, 2))
 main()
