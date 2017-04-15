443874b4abe73e9cf774ab9522b6783ffc749f5f
RegisMelgaco <regis.melgaco@gmail.com>
2017-04-15 10:27:31 -0300

    x_generator() na main

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def main():
     text = scn.textinput("Teste", "Teste Teste")
     print(helper.get_y(text, 2))
 main()
+
+
+def x_generator():
+    num_x=turtle.window_width()/2
+    a=(num_x/2)*(-1)
+    x=[]
+    for i in range(int(num_x)):
+        x.append(a)
+        a+=1
+    return x
+    
