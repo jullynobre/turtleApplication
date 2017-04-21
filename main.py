ea62ad6c70c9421d00061f9570d2dd1e091ade1e
Jully Nobre <jullynobre1@gmail.com>
2017-04-21 16:31:38 -0300

    Teste 2238209
    testando

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import helper
 import draw
 import ctypes
 
-scale=10
+scale = 10
 
 
 def x_generator():
@@ -19,44 +19,40 @@ def x_generator():
 def y_generator(x, func):
     y = []
     for i in range(len(x)):
-        j=helper.get_y(func, x[i])
+        j = helper.get_y(func, x[i])
         y.append(float(j))
     return y
 
 
 def som_xy(x, y):
-    
     raw_coordinates = []
     for i in range(len(x)):
-        
-
-
-
         raw_coordinates.append(x[i])
         raw_coordinates.append(y[i])
     return raw_coordinates
 
 
 def upscale():
-    scale+=5
+    global scale
+    scale += 5
     main()
 
 
 def main():
     scn = turtle.Screen()
     screensize = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
-    turtle.setup(screensize[0],screensize[1])
+    turtle.setup(screensize[0], screensize[1])
     turtle.title("Grafic Builder 1.0")
     text = scn.textinput("Grafic Builder 1.0", "Informe a função")
     x = x_generator()
     y = y_generator(x, text)
     raw_coordinates = som_xy(x, y)
     draw.draw(raw_coordinates, scale)
-##    turtle.onkey(upscale(scale,raw_coordinates),'p')
-##    turtle.listen()
+    # turtle.onkey(upscale(), 'p')
+    # turtle.listen()
     turtle.exitonclick()
 
 main()
-turtle.onkey(upscale(),'p')
+turtle.onkey(upscale(), 'p')
 turtle.listen()
 turtle.exitonclick()
